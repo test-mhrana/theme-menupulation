38c04b8b53635c05603899584a3703461d01090a
Rana-ERP-67 <Rana-ERP-67@RANA-ERP-67>
2024-02-20 17:57:28 +0600

    Header Profile Design

--- src/app/layout/shared-component/profile/profile.component.ts
+++ src/app/layout/shared-component/profile/profile.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import {ListboxModule} from "primeng/listbox";
+import {PaginatorModule} from "primeng/paginator";
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+    selector: 'app-profile',
+    standalone: true,
+    imports: [
+        OverlayPanelModule,
+        ListboxModule,
+        ButtonModule,
+        PaginatorModule
+    ],
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+    countries!: any[];
+    selectedCountry!: any;
 
+    ngOnInit() {
+        this.countries = [
+            {name: 'Australia', code: 'AU'},
+            {name: 'Brazil', code: 'BR'},
+            {name: 'China', code: 'CN'},
+            {name: 'Egypt', code: 'EG'},
+            {name: 'France', code: 'FR'},
+            {name: 'Germany', code: 'DE'},
+            {name: 'India', code: 'IN'},
+            {name: 'Japan', code: 'JP'},
+            {name: 'Spain', code: 'ES'},
+            {name: 'United States', code: 'US'}
+        ];
+    }
+
+    clickOption(event) {
+        console.warn(event)
+    }
 }

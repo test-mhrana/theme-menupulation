200357d45516b6b62a49d82e805f824825d4d9bd
Rana-ERP-67 <Rana-ERP-67@RANA-ERP-67>
2024-02-22 11:47:15 +0600

    Profile List Box Change CSS

--- src/app/layout/shared-component/profile/profile.component.ts
+++ src/app/layout/shared-component/profile/profile.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {OverlayPanelModule} from 'primeng/overlaypanel';
-import {ListboxModule} from "primeng/listbox";
+import {Listbox, ListboxModule} from "primeng/listbox";
 import {PaginatorModule} from "primeng/paginator";
 
 @Component({
@@ -18,7 +18,7 @@ import {PaginatorModule} from "primeng/paginator";
 })
 export class ProfileComponent implements OnInit {
     countries!: any[];
-    selectedCountry!: any;
+    @ViewChild('myListbox') myListboxComponent: Listbox;
 
     ngOnInit() {
         this.countries = [
@@ -26,17 +26,12 @@ export class ProfileComponent implements OnInit {
             {name: 'Brazil', code: 'BR'},
             {name: 'China', code: 'CN'},
             {name: 'Egypt', code: 'EG'},
-            {name: 'France', code: 'FR'},
-            {name: 'Germany', code: 'DE'},
-            {name: 'India', code: 'IN'},
-            {name: 'Japan', code: 'JP'},
-            {name: 'Spain', code: 'ES'},
-            {name: 'United States', code: 'US'}
+            {name: 'France', code: 'FR'}
         ];
     }
 
     clickOption(event) {
-        console.warn(event.option.code);
-        this.selectedCountry = null;
+        this.myListboxComponent.modelValue.set(null);
+        console.warn(event.value);
     }
 }
